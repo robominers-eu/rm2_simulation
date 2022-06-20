2607e047aa2615915ca1ee1f16683964b7c124a9
PedroSoler10 <soler.pedrojavier@gmail.com>
2022-06-20 15:54:21 +0200

    Teleoperation included

--- launch/rm2_wheels.launch.py
+++ launch/rm2_wheels.launch.py
@@ -15,6 +15,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     pkg = get_package_share_directory('rm2_simulation')
+    use_sim_time = LaunchConfiguration('use_sim_time')
     
     ign_gazebo = IncludeLaunchDescription(
       PythonLaunchDescriptionSource(
@@ -23,15 +24,34 @@ def generate_launch_description():
      )
 
     spawn = Node(
-                  package='ros_ign_gazebo',
-                  executable='create',
+                  package='ros_ign_gazebo', executable='create',
                   arguments=[
-                    '-name', 'rm2_simple',
+                    '-name', 'rm2_wheels',
                     '-file',  os.path.join(pkg, 'models', 'rm2_wheels', 'model.sdf'),
                     '-z', '0.5',
                     ],
                 output='screen',
                 )
+    
+    bridge = Node(
+      package='ros_ign_bridge',
+      executable='parameter_bridge',
+      arguments=[
+      # Mappings (IGN -> ROS2)
+      # The ROS message type is followed by an @, [, or ] symbol where:
+
+      # @ is a bidirectional bridge.
+      # [ is a bridge from Ignition to ROS.
+      # ] is a bridge from ROS to Ignition.
+      # https://github.com/ignitionrobotics/ros_ign/tree/ros2/ros_ign_bridge
+
+      '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',                    
+             
+                ],
+      remappings=[
+            ('/cmd_vel', 'cmd_vel'),
+        ],
+        output='screen')
     
     return LaunchDescription([
       DeclareLaunchArgument(
@@ -39,5 +59,6 @@ def generate_launch_description():
           default_value=[os.path.join(pkg, 'worlds', 'empty.sdf')]),
         ign_gazebo,
         spawn,
+        bridge
     ])
 
